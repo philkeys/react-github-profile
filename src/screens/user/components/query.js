0ff80da5ae6c7e42e64c269e68bec8096ae84a60
Phil Keys <phil@phillipkeys.com>
2018-12-16 11:39:22 -0500

    refactor to use useRef and useEffect to do a deep equal on the props to see if we actually need to run the query

--- src/screens/user/components/query.js
+++ src/screens/user/components/query.js
@@ -1,5 +1,6 @@
-import {useContext, useReducer, useEffect} from 'react'
+import {useContext, useReducer, useEffect, useRef} from 'react'
 import PropTypes from 'prop-types'
+import isEqual from 'lodash/isEqual'
 import {Context} from '../../../github-client'
 
 function Query({query, variables, children, normalize = data => data}) {
@@ -9,30 +10,36 @@ function Query({query, variables, children, normalize = data => data}) {
     {loaded: false, fetching: false, data: null, error: null},
   )
   // Note - useEffect is called every time our component is updated.
-  useEffect(
-    () => {
-      setState({fetching: true})
-      client
-        .request(query, variables)
-        .then(res =>
-          setState({
-            data: normalize(res),
-            error: null,
-            loaded: true,
-            fetching: false,
-          }),
-        )
-        .catch(error =>
-          setState({
-            error,
-            data: null,
-            loaded: false,
-            fetching: false,
-          }),
-        )
-    },
-    [query, variables],
-  )
+  useEffect(() => {
+    if (isEqual(previousInputs.current, [query, variables])) {
+      return
+    }
+    setState({fetching: true})
+    client
+      .request(query, variables)
+      .then(res =>
+        setState({
+          data: normalize(res),
+          error: null,
+          loaded: true,
+          fetching: false,
+        }),
+      )
+      .catch(error =>
+        setState({
+          error,
+          data: null,
+          loaded: false,
+          fetching: false,
+        }),
+      )
+  })
+
+  const previousInputs = useRef()
+  useEffect(() => {
+    previousInputs.current = [query, variables]
+  })
+
   return children(state)
 }
 
